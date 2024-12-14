ffd777ca34224f93738886b8d12b6219fa4095b7
Ansh <agnihotriansh07@gmail.com>
2024-12-14 19:19:02 +0530

    JournalPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,15 +3,17 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Footer from './Components/Footer/Footer'
+import Journals from './Components/Pages/Journals/Journals'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <BrowserRouter>
     <Routes >
       <Route path='/' element={<Footer/>} />
-
+      <Route path='/journals' element={<Journals/>} />
 
     </Routes>
 
@@ -20,7 +22,7 @@ function App() {
     </BrowserRouter>
     
   
-      
+    </>
   )
 }
 
