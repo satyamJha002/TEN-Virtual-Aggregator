5c17e06af350698a2a04e6979f6e04384f0861ae
Anjish <136132607+anjishkr@users.noreply.github.com>
2024-12-18 19:34:21 +0530

    navbarandfooter

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
-
 import './App.css'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Footer from './Components/Footer/Footer'
 
+import VirtualNavbar from './Components/Navbar/Navbar'
+import ContactUs from './Components/Pages/ContactUs/ContactUs'
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+    {/* <ScrollToTop/> */}
+    <VirtualNavbar />
     <Routes >
-      <Route path='/' element={<Footer/>} />
+   
+
+  
+    <Route path='/ContactUs' element={<ContactUs/>} />
+    
+      
 
 
     </Routes>
+    <Footer />
 
 
 
