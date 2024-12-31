9e6aa34a000a8476e95e8a4c0994273a82c0b664
satyamJha002 <satyam073000@gmail.com>
2024-12-31 15:08:57 +0530

    vercel and module change

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
